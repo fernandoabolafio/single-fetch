62bfe34166b18726b740c239108a02f8eed33dc7
Fernando Abolafio <fernandohsabolafio@gmail.com>
2024-06-13 13:23:51 +0200

    add teletransport

--- app/routes/location.$country.tsx
+++ app/routes/location.$country.tsx
@@ -4,28 +4,80 @@ import { LoaderFunctionArgs } from "@remix-run/node";
 import { Link, Outlet, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
 export function loader({ params }: LoaderFunctionArgs) {
   invariant(params.country, "Country is required");
   const countryNameAndFlag = getCountryNameAndFlag(params.country);
-  return { countryNameAndFlag, country: params.country};
+  return { countryNameAndFlag, country: params.country };
 }
 
 // display the country with a flag
 export default function Country() {
-  const { countryNameAndFlag, country  } = useLoaderData<typeof loader>();
+  const { countryNameAndFlag, country } = useLoaderData<typeof loader>();
 
   return (
     <main className="flex w-full justify-center">
       <Card className="w-[800px]">
-        <CardHeader>
+        <CardHeader className="flex justify-between w-full flex-row">
           <CardTitle className="capitalize text-xl">
-            <Link to={`/location/${country}/regions` }> {countryNameAndFlag} </Link>
+            <Link to={`/location/${country}/regions`}>
+              {" "}
+              {countryNameAndFlag}{" "}
+            </Link>
           </CardTitle>
+          <Teletransport />
         </CardHeader>
         <CardContent>
           <Outlet />
         </CardContent>
       </Card>
     </main>
+  );
+}
+
+export function Teletransport() {
+  return (
+    <Menubar >
+      <MenubarMenu>
+        <MenubarTrigger>Teletransport to</MenubarTrigger>
+        <MenubarContent>
+          <MenubarSub>
+            <MenubarSubTrigger>Brazil</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem>
+                <Link to={'/location/brazil/regions/north'}>North</Link>
+              </MenubarItem>
+              <MenubarItem>
+                <Link to={'/location/brazil/regions/northeast'}>Northeast</Link>
+              </MenubarItem>
+              <MenubarItem>
+                <Link to={'/location/brazil/regions/central-west'}>Central West</Link>
+              </MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+          <MenubarSub>
+            <MenubarSubTrigger>Denmark</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem>
+                <Link to={'/location/denmark/regions/zealand'}>Zealand</Link>
+              </MenubarItem>
+              <MenubarItem>
+                <Link to={'/location/denmark/regions/jutland'}>Jutland</Link>
+              </MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
   );
 }
